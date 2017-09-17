5b4efa101f584b02445243f17cd5d65ee5d338fa
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    msm8996-common: Add basic USB HAL that reports no status change
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff

--- msm8996.mk
+++ msm8996.mk
@@ -341,6 +341,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.xiaomi_8996
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
