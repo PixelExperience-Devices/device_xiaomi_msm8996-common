413754492c6bdf1d3450cdf42ccd3c5eac37e998
rk2810 <rohan.kumquat@gmail.com>
2018-01-16 06:37:52 +0530

    msm8996-common: Remove lineage dependencies from PocketMode

--- pocketmode/src/com/cyanogenmod/pocketmode/ProximitySensor.java
+++ pocketmode/src/com/cyanogenmod/pocketmode/ProximitySensor.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.lineageos.pocketmode;
+package com.cyanogenmod.pocketmode;
 
 import android.content.Context;
 import android.hardware.Sensor;
@@ -24,7 +24,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
-import org.lineageos.internal.util.FileUtils;
+import com.cyanogenmod.pocketmode.utils.FileUtils;
 
 public class ProximitySensor implements SensorEventListener {
 
