9d942b7fd51c23f8549e4761236f3f3d48ef1328
Sebastian <xdzienny@gmail.com>
2018-04-28 12:55:20 +0000

    msm8996-common: More EAS changes
    Signed-off-by: Sebastian <xdzienny@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -303,7 +303,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.0-service.gemini
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
