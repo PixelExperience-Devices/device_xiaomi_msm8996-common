fafb14a9a2177fc36acf59911d5b0558b9b8ab8e
PsyMan47 <xhonirexhepi@gmail.com>
2018-05-06 12:39:02 +0200

    msm8996-common: Remove overlay-lineage

--- msm8996.mk
+++ msm8996.mk
@@ -23,8 +23,7 @@ $(call inherit-product, vendor/xiaomi/msm8996-common/msm8996-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
