5176973c112d3072cfa129b538d956d9134cd50a
Alberto Guanti <albertoguanti@gmail.com>
2018-05-14 10:19:53 +0200

    msm8996-common: Build thermal HIDL interface

--- msm8996.mk
+++ msm8996.mk
@@ -359,6 +359,11 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
