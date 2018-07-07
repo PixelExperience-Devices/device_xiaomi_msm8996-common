c8acbaf4c4868f2cfec01dcab4af2577abf65e76
Bruno Martins <bgcngm@gmail.com>
2018-07-07 22:40:59 +0100

    msm8996-common: Remove implicitly built QTI camera shared libraries
     * These shared libs are built when TARGET_USES_QTI_CAMERA_DEVICE
       flag is enabled.
    
    Change-Id: Ifb39b7c2c8de2cbb1f59c6dc492a3763d32f1b7a

--- msm8996.mk
+++ msm8996.mk
@@ -131,8 +131,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8996 \
     libmm-qcamera \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
 
 # CNE
 PRODUCT_PACKAGES += \
