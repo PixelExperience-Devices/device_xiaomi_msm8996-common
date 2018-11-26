5532f17d766a0b1a995d84e4684aad6bc2d761d6
Wileen Chiu <wileenc@codeaurora.org>
2018-11-26 12:08:55 -0800

    msm8996-common: Add support for telephony hidl wrapper
    Change-Id: I4e248bb6de482d503e672d284189b24cafac8d20

--- msm8996.mk
+++ msm8996.mk
@@ -373,6 +373,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
     telephony-ext
