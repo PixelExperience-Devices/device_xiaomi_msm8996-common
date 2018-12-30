b93dfe6bf43444fbf8eac918dd31874ddb284a23
Henrique Pereira <pereirah2001@gmail.com>
2018-12-30 20:46:12 +0000

    Revert "msm8996-common: Kang WFD blobs from Mi A2"
    This reverts commit b8eb6e91c524b3ccf7dc81dabf51299aa2ce0e19.

--- msm8996.mk
+++ msm8996.mk
@@ -410,7 +410,3 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# Wfd
-PRODUCT_BOOT_JARS += \
-    WfdCommon
