50386a5415dcdac06542a9f7f4e3b56357e47db7
argraur <argraur@gmail.com>
2019-04-22 15:41:43 +0000

    msm8996-common: power-libperfmgr: InteractionHandler: exit silently if int state is uninitialized
    Silence this warning:
    android.hardware.power@1.3-service.xiaomi-libperfmgr: Acquire: called while uninitialized
    
    Signed-off-by: argraur <argraur@gmail.com>

--- power-libperfmgr/InteractionHandler.cpp
+++ power-libperfmgr/InteractionHandler.cpp
@@ -113,10 +113,8 @@ long long InteractionHandler::CalcTimespecDiffMs(struct timespec start,
 void InteractionHandler::Acquire(int32_t duration) {
 
     std::lock_guard<std::mutex> lk(mLock);
-    if (mState == INTERACTION_STATE_UNINITIALIZED) {
-        ALOGW("%s: called while uninitialized", __func__);
+    if (mState == INTERACTION_STATE_UNINITIALIZED)
         return;
-    }
 
     int inputDuration = duration + 650;
     int finalDuration;
